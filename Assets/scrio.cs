cebfb3c157e43ec2dae8b0ea15a79ea7dc886a69
ZEYU LI <zli013@campus.goldsmiths.ac.uk>
2025-11-23 20:23:33 +0000

    test1
    111

---
+++ Assets/scrio.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class scrio : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    private CharacterController controller;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        // 获取输入（-1~1）
+        float h = Input.GetAxisRaw("Horizontal");  // A, D / 左右键
+        float v = Input.GetAxisRaw("Vertical");    // W, S / 上下键
+
+        // 输入方向向量
+        Vector3 dir = new Vector3(h, 0, v).normalized;
+
+        // 若有输入，执行移动
+        if (dir.magnitude >= 0.1f)
+        {
+            controller.Move(dir * moveSpeed * Time.deltaTime);
+        }
+    }
+}
