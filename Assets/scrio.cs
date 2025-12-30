3d527a38527a25a315d30f9a14d684c8b27cb757
ZEYU LI <zli013@campus.goldsmiths.ac.uk>
2025-12-30 19:42:58 +0000

    PlayerMovement
    PlayerMovement

--- Assets/scrio.cs
+++ Assets/scrio.cs
@@ -1,29 +1,53 @@
 using UnityEngine;
+using UnityEngine.AI;
+
 
 public class scrio : MonoBehaviour
 {
+    [Header("PlayerData")]
     public float moveSpeed = 5f;
+    public float acceleration = 10f;
+    public float angularSpeed = 720f;
+    public float stoppingDistance = 0f;
 
-    private CharacterController controller;
+    [Header("Arrow")]
+    public GameObject Arrow;
+    public float ClickInterval;
+    public float TargetClickInterval;
 
+    public Camera Cam;
+
+    public NavMeshAgent Agent;
     void Start()
     {
-        controller = GetComponent<CharacterController>();
+        Agent.speed = moveSpeed;
+        Agent.acceleration = acceleration;
+        Agent.angularSpeed = angularSpeed; 
+        Agent.stoppingDistance = stoppingDistance;
     }
 
     void Update()
     {
-        // 获取输入（-1~1）
-        float h = Input.GetAxisRaw("Horizontal");  // A, D / 左右键
-        float v = Input.GetAxisRaw("Vertical");    // W, S / 上下键
+        ClickInterval += Time.unscaledDeltaTime;
 
-        // 输入方向向量
-        Vector3 dir = new Vector3(h, 0, v).normalized;
-
-        // 若有输入，执行移动
-        if (dir.magnitude >= 0.1f)
+        if(ClickInterval>= TargetClickInterval)
         {
-            controller.Move(dir * moveSpeed * Time.deltaTime);
+           ClickInterval = TargetClickInterval;
+        }
+
+        if (Input.GetMouseButtonDown(1)&& ClickInterval>=TargetClickInterval)
+        {
+            Ray ray = Cam.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            ClickInterval = 0;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                GameObject arrowObj = Instantiate(Arrow, hit.point + Vector3.up * 0.1f, Quaternion.identity);
+                Destroy(arrowObj, 0.5f);
+                Agent.SetDestination(hit.point);
+            }
         }
     }
 }
