dc1517db52223040ed5fcaba43f4e8f57c3e1d79
ZEYU LI <zli013@campus.goldsmiths.ac.uk>
2026-01-11 08:19:17 +0000

    Update PickUpItem.cs

--- Assets/PickUpItem.cs
+++ Assets/PickUpItem.cs
@@ -3,10 +3,10 @@ using UnityEngine;
 
 public class PickUpItem : MonoBehaviour
 {
-    //移动速度
-    public float moveSpeed = 5f;
+    //Speed
+    public float moveSpeed = 20f;
     
-    //移动目标
+   
     public Transform target;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -19,9 +19,9 @@ public class PickUpItem : MonoBehaviour
     {
         if (target)
         {
-            //匀速向目标移动
+           
             transform.position = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
-            //到达目标点后销毁
+            
             if (transform.position == target.position)
             {
                 Destroy(gameObject);
